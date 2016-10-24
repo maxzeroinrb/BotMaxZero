4d39775bc8fc97b906fb53ec1da3a4d29fac25bb
MaxZero-MacBook <maxzero-macbook@MaxZero-MacBooks-MacBook-Pro.local>
2016-10-24 18:19:00 +0700

    no message

--- botMaxZero.php
+++ botMaxZero.php
@@ -20,9 +20,18 @@ if (!is_null($events['events'])) {
 	 						$text = "ว่าอะไรนะ";
 	 					}
 	 					
-	 					$messages = [
-							'type' => 'text',
-							'text' => $text
+	 					$messages = 
+	 						[
+	 							{
+								'type' => 'text',
+								'text' => $text
+		 						},
+		 						{
+		 						'type' => 'sticker',
+								'packageId' => '3',
+								'stickerId' => '180'
+		 						}
+	
 							];
 					break;
 					case 'image':
@@ -66,62 +75,7 @@ if (!is_null($events['events'])) {
 
 			echo $result . "\r\n";
 		}
-			if($event['type'] == 'message'){
- 				switch($event['message']['type']){
-	 				case 'text':
-	 					if($event['message']['text'] == "/help"){
-	 						$text = "คุณสามารถ พิมพ์ว่า /help เพื่อลิสดูคำสั่งที่สามารถทำงานให้คุณได้ค่ะ";
 
-	 					}else{
-	 						$text = "ว่าอะไรนะ";
-	 					}
-	 					
-	 					$messages = [
-							'type' => 'text',
-							'text' => $text
-							];
-					break;
-					case 'image':
-
-					break;
-					 case 'video':
-					 
-					 break;
-					 case 'audio':
-					 
-					 break;
-					 case 'location':
-
-					break;
-					 case 'sticker':
-					 		$messages = [
-							'type' => 'sticker',
-							'packageId' => '3',
-							'stickerId' => '180'
-	 							];
-					 break;
-			    }
-			  $replyToken = $event['replyToken'];  
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
-
-			echo $result . "\r\n";
-		}
 	}
 }
 echo "OK";
