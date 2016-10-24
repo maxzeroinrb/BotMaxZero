07bde50945a75753236af2188d301a707ee40b45
MaxZero-MacBook <maxzero-macbook@MaxZero-MacBooks-MacBook-Pro.local>
2016-10-24 17:29:22 +0700

    no message

--- botMaxZero.php
+++ botMaxZero.php
@@ -59,31 +59,31 @@ if (!is_null($events['events'])) {
 
 					 break;
 			    }
-			  $replyToken = $event['replyToken'];  
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-				$messages = [
-							'type' => 'sticker',
-							'packageId' => '3',
-							'stickerId' => '180'
-	 							]
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+			//   $replyToken = $event['replyToken'];  
+			// // Make a POST Request to Messaging API to reply to sender
+			// $url = 'https://api.line.me/v2/bot/message/reply';
+			// 	$messages = [
+			// 				'type' => 'sticker',
+			// 				'packageId' => '3',
+			// 				'stickerId' => '180'
+	 	// 						]
+			// $data = [
+			// 	'replyToken' => $replyToken,
+			// 	'messages' => [$messages],
+			// ];
+			// $post = json_encode($data);
+			// $headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+			// $ch = curl_init($url);
+			// curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			// curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			// curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			// curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			// $result = curl_exec($ch);
+			// curl_close($ch);
 
-			echo $result . "\r\n";
+			// echo $result . "\r\n";
 		}
 	}
 }
