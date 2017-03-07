710bcfae9fda963ef24d4742100af4ca7c737c60
wittaya jitsuwan <maxzerozity@gmail.com>
2017-03-07 16:55:00 +0700

    no message

--- botMaxZero.php
+++ botMaxZero.php
@@ -13,13 +13,14 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
+			$urlcard  = getcard($text);
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
 			// Build message to reply back
 			$messages = [
 				'type' => 'text',
-				'text' => $text
+				'text' => $urlcard
 			];
 
 			// Make a POST Request to Messaging API to reply to sender
@@ -44,4 +45,48 @@ if (!is_null($events['events'])) {
 		}
 	}
 }
-echo "OK";
+function getcard($name){
+
+	$content = file_get_contents('php://input');
+
+			// Make a POST Request to Messaging API to reply to sender
+			$url = 'http://cardfight.wikia.com/api/v1/Search/List?query=';
+			$limit = '&limit=1';
+			$cardname = $name;
+
+			$urlquerycard = $url.$cardname.$limit;
+			// $data = [
+			// 	'replyToken' => $replyToken,
+			// 	'messages' => [$messages],
+			// ];
+			// $post = json_encode($data);
+			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+
+			$ch = curl_init($urlquerycard);
+			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+			$result = curl_exec($ch);
+			curl_close($ch);
+			$obj = json_decode($result);
+			$arrayitem =  $obj->items;
+			$cardlist = $arrayitem[0]->url;
+
+		return $cardlist;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
