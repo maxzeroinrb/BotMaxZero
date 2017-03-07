4c3a78cad91fd7c7b1af71e22433315c2deb8bfe
wittaya jitsuwan <maxzerozity@gmail.com>
2017-03-07 17:48:12 +0700

    ดักภาษา

--- botMaxZero.php
+++ botMaxZero.php
@@ -13,35 +13,43 @@ if (!is_null($events['events'])) {
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
 			$text = $event['message']['text'];
-			$urlcard  = getcard($text);
-			// Get replyToken
-			$replyToken = $event['replyToken'];
+			 if(strlen($cardname) != mb_strlen($cardname, 'utf-8'))
+			    { 
+			        // echo "Please enter English words only:(";
+			    }
+			    else {
+			       			$urlcard  = getcard($text);
 
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $urlcard
-			];
+							// Get replyToken
+							$replyToken = $event['replyToken'];
 
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
+							// Build message to reply back
+							$messages = [
+								'type' => 'text',
+								'text' => $urlcard
+							];
 
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
+							// Make a POST Request to Messaging API to reply to sender
+							$url = 'https://api.line.me/v2/bot/message/reply';
+							$data = [
+								'replyToken' => $replyToken,
+								'messages' => [$messages],
+							];
+							$post = json_encode($data);
+							$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
 
-			echo $result . "\r\n";
+							$ch = curl_init($url);
+							curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+							curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+							curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
+							curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+							curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+							$result = curl_exec($ch);
+							curl_close($ch);
+
+							echo $result . "\r\n";
+			    }
+
 		}
 	}
 }
